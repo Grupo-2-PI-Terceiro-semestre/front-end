7d9d37609427f44d57eac37c037f37f4b8b810d0
Jonathan Carvalho <jonathan.carvalho@cxpbrasil.com.br>
2024-09-06 09:41:52 -0300

    Ajustes no index

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import LoginForm from '../../components/LoginForm/LoginForm';
-/* import './Login.css';
- */
+
 const Login = () => {
   return (
     <div className="login-container">
-      <LoginForm />
+     <h1>Login</h1>
     </div>
   );
 };
