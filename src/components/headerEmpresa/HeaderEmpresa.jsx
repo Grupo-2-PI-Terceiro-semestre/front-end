b68a8b735e2f242598e8679001552a2ed8b275eb
Kaiquevolpe <kaique.silva@sptech.school>
2024-09-27 20:05:43 -0300

    Atualização header home cliente

--- src/components/headerEmpresa/HeaderEmpresa.jsx
+++ src/components/headerEmpresa/HeaderEmpresa.jsx
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './HeaderEmpresa.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 import { faInstagram, faFacebookF, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
-function HeaderEmpresa({ navLinks, buttonText, width, widthOpcoes }) {  // Adiciona width como prop
+function HeaderEmpresa({ navLinks, buttonText, width, widthOpcoes }) {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 50) { 
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <header className='headerEmpresa'>
+        <header className={`headerEmpresa ${isScrolled ? 'headerScroll' : ''}`}>
             <div className='icones'>
                 <FontAwesomeIcon icon={faInstagram} className="icon" />
                 <FontAwesomeIcon icon={faFacebookF} className="icon" />
                 <FontAwesomeIcon icon={faYoutube} className="icon" />
             </div>
 
-            {/* Aplica o width passado como prop */}
-            <div className='titulos' style={{ width: width }}>  
+            <div className='titulos' style={{ width: width }}>
                 <span>Order Hub</span>
-                <div className='opcoes' style={{ width: widthOpcoes}}>
+                <div className='opcoes' style={{ width: widthOpcoes }}>
                     <nav className='nav'>
                         {navLinks.map((link, index) => (
                             <a href={link.url} key={index}>{link.name}</a>
