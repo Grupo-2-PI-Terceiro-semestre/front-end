f55e67a17f63c50590ba0b86f8d1d31cc4043bd3
JonathanCarvalho39 <jonathan.carvalho@sptech.school.com>
2024-09-28 19:32:00 -0300

    Atualizando projeto

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,15 @@ export default defineConfig({
   plugins: [react()],
   build: {
     target: 'esnext',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Dividir módulos da pasta node_modules em chunks separados
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
   },
 })
-
-
-
