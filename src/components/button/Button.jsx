eba329eeed0c117a0d011aa8de49b07dca603450
JonathanCarvalho39 <jonathan.carvalho@sptech.school.com>
2024-09-29 17:43:00 -0300

    Ajuste no Componente Botao

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import './Button.css';
+import { height, width } from '@fortawesome/free-brands-svg-icons/fa42Group';
 
 const Button = ({
+  fontSize = '1rem',
+  height = '2rem',
+  widthImage = '',
+  heightImage = '',
   size = '100%',
   padding = '10px',
   backgroundColor = '#0072FF',
@@ -17,6 +22,8 @@ const Button = ({
       className="dynamic-button"
       type={type}
       style={{
+        fontSize: fontSize,
+        height: height,
         width: size,
         padding: padding,
         backgroundColor: backgroundColor,
@@ -25,7 +32,7 @@ const Button = ({
       }}
       onClick={onClick}
     >
-      {image && <img src={image} alt="button-icon" className="button-image" />}
+      {image && <img style={{width: widthImage, height: heightImage}} src={image} alt="button-icon" className="button-image" />}
       {content}
     </button>
   );
