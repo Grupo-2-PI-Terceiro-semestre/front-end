ef75d736d928faa98c1611b88aaa16f6c0b72d79
JonathanCarvalho39 <jonathan.carvalho@sptech.school.com>
2024-09-29 17:42:45 -0300

    Criando Componente Agenda

---
+++ src/pages/Agenda/components/agenda/Agenda.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Calendar } from 'primereact/calendar';
+import { addLocale } from 'primereact/api';
+import './style.css'; // Certifique-se de que este arquivo tenha as classes CSS
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
+
+// Configuração da localidade para o Brasil
+addLocale('br', {
+    firstDayOfWeek: 1,
+    dayNames: ['domingo', 'segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado'],
+    dayNamesShort: ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sáb'],
+    dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
+    monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+    monthNamesShort: ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez'],
+    today: 'Hoje',
+    clear: 'Limpar'
+});
+
+export default function IconDemo({content, onDateChange }) {
+    const [date, setDate] = useState(null); // Estado local para armazenar a data
+
+    const handleDateChange = (e) => {
+        setDate(e.value); // Atualiza o estado local com a data selecionada
+        onDateChange(e.value); // Passa a data para o componente pai
+    };
+
+    return (
+        <div className="calendar-input">
+            <label htmlFor="buttondisplay" className="font-bold block mb-2">
+                Agenda
+            </label>
+            <Calendar
+                value={date} // O valor do calendário está diretamente ligado ao estado
+                onChange={handleDateChange} // Atualiza a data
+                locale="br"
+                id="buttondisplay"
+                className="custom-calendar" // A classe que aplica a transição
+                placeholder={content}
+                dateFormat="dd/mm/yy"
+                inputStyle={{ width: '100%' }} // Faz o input ocupar toda a largura
+            />
+        </div>
+    );
+}
