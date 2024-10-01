a7b9094e5dbc391fc33691ed49c28386f2723e60
JonathanCarvalho39 <jonathan.carvalho@sptech.school.com>
2024-10-01 20:59:55 -0300

    implementando logica da agenda

--- src/pages/Agenda/Agenda.jsx
+++ src/pages/Agenda/Agenda.jsx
@@ -1,11 +1,21 @@
 import MyCalendar from './components/calendar/MyCalendar';
+import MenuPerfil from '../../components/menu/Menu';
+import './Agenda.css';
 
 const Agenda = () => {
   return (
-    <div>
-      <MyCalendar />
+    <div className="main-servico">
+      <div className="container-servico">
+        <div className="menu-lateral">
+          <MenuPerfil />
+        </div>
+        <div className="principal">
+          <MyCalendar />
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Agenda;
+
