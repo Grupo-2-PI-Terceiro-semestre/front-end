b46241350deb240f29615a611269f5eaf451d220
JonathanCarvalho39 <jonathan.carvalho@sptech.school.com>
2024-10-05 17:30:32 -0300

    implementando o IconBreadcrumbs na agenda e no servico

--- src/pages/Agenda/Agenda.jsx
+++ src/pages/Agenda/Agenda.jsx
@@ -1,15 +1,23 @@
 import MyCalendar from './components/calendar/MyCalendar';
-import MenuPerfil from '../../components/menu/Menu';
+import Menu from '../../components/menu/Menu';
+import IconBreadcrumbs from '../../components/breadcrumb/Breadcrumb';
 import './Agenda.css';
 
 const Agenda = () => {
+  const activeMenuItem = 'Agenda'; // Define qual item está ativo no menu
+
   return (
     <div className="main-servico">
       <div className="container-servico">
         <div className="menu-lateral">
-          <MenuPerfil />
+          <Menu activeMenuItem={activeMenuItem} />
         </div>
         <div className="principal">
+          <IconBreadcrumbs
+            paths={[
+              { label: 'Agenda', href: '/agenda' }
+            ]}
+          />
           <MyCalendar />
         </div>
       </div>
