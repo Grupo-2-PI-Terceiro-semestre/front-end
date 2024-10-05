80ed12a77da68fa7ee1746a4c8596ebfd0632d0b
JuliaIFreitas <julia.freitas@sptech.school>
2024-10-05 20:42:53 -0300

    Criei o modal de excluir

---
+++ src/components/modalExcluir/ModalExcluir.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import './ModalExcluir.css';
+
+function ModalExcluir({ onClose }) {
+    return (
+        <div className="modal-tela">
+            <div className="container-modal">
+                <div className="modal-inside">
+                    <div className="simbolos">
+                        <div className="imagem">
+                            <img src="../../assets/simboloAtencao.png" alt="" />
+                        </div>
+                        <button className="botaoFechar" onClick={onClose}>X</button>
+                    </div>
+                    <div className="textos">
+                        <label className="titulo-atencao" htmlFor="text"><b>Atenção!</b></label>
+                        <label htmlFor="text">Tem certeza que deseja excluir esse serviço?</label>
+                    </div>
+
+                    <div className="botoes">
+                        <button className="cancelar" onClick={onClose}>Cancelar</button>
+                        <button className="excluir" >Excluir</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ModalExcluir;
