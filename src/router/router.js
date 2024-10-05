2e313a6536e42137e7b27b9f961a3381f1446007
JonathanCarvalho39 <jonathan.carvalho@sptech.school.com>
2024-10-05 17:33:25 -0300

    adicionando varivel de ambiente

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const API_URL = 'http://localhost:8080/api/v1/';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080/api/v1/';
+
 
 export const getData = async (endpoint, pathParams = {}, queryParams = {}) => {
     try {
