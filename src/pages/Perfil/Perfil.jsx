a0160a59a8b3a82d7f54a00963263493b494c82b
Kaiquevolpe <kaique.silva@sptech.school>
2024-10-06 18:45:50 -0300

    Alinhamento tela perfil

--- src/pages/Perfil/Perfil.jsx
+++ src/pages/Perfil/Perfil.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import "./Perfil.css";
 import MenuPerfil from "../../components/menuPerfil/MenuPerfil";
+import BotoesPerfil from "../../components/botoesPerfil/BotoesPerfil";
+import FormularioPrincipal from "../../components/formularioDadosPrincipais/FormularioPrincipal";
+
 
 function Perfil() {
     return (
-        <div className="main-perfil"> 
-        <MenuPerfil />
+        <div className="main-servico">
+            <div className="container-perfil">
+                <div className="menu-lateral">
+                    <MenuPerfil />
+                </div>                
+                <div className="principal">
+                   <BotoesPerfil />
+                   <FormularioPrincipal />
+                </div>
+               
+            </div>
         </div>
-
     );
 }
 
