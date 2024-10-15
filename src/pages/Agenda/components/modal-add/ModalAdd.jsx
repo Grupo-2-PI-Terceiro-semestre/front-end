03b67109667688a10cb95372d1db65a16ee9a7d9
JonathanCarvalho39 <jonathan.carvalho@sptech.school.com>
2024-10-15 20:36:02 -0300

    Corrigindo modall add

--- src/pages/Agenda/components/modal-add/ModalAdd.jsx
+++ src/pages/Agenda/components/modal-add/ModalAdd.jsx
@@ -1,108 +1,167 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import './ModalAdd.css';
+import { findServicos, findClientes } from '../../services/agendaServices';
+import SearchableDropdown from '../autocomplete/SearchableDropdown';
+import DateTimePickerOpenTo from '../input-horas/DateTimePickerOpenTo';
+import Button from '../../../../components/button/Button';
+import dayjs from 'dayjs';
 
-function ModalAdd({ onClose }) {
 
-    const [formData, setFormData] = useState({
-        nomeServico: '',
-        valorServico: '',
-        tempoExecucao: '',
-        categoria: '',
-        tiposDeUsuario: 'ADMIN'
-    });
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
+
+function ModalAdd({ onClose, idEmpresa, funcionarios, dateDefault }) {
+
+    const [servicos, setServicos] = useState([]);
+    const [clientes, setClientes] = useState([]);
+    const [servicoSelecionado, setServicoSelecionado] = useState('');
+    const [clienteSelecionado, setClienteSelecionado] = useState('');
+    const [profissionalSelecionado, setProfissionalSelecionado] = useState('');
+    const [dataHoraAgendamento, setDataHoraAgendamento] = useState('');
+    const [isVisible, setIsVisible] = useState(false);
+    const dropdownRef = useRef(null);
+
+
+
+    useEffect(() => {
+        buscarServicos(idEmpresa);
+        buscarClientes(idEmpresa)
+        setIsVisible(true);
+        setDataHoraAgendamento(dateDefault);
+    }, [idEmpresa]);
+
+    const handleServicoChange = (servico) => {
+        setServicoSelecionado(servico.nomeServico);
+    };
+
+    const handleFuncionarioChange = (funcionario) => {
+        setProfissionalSelecionado(funcionario.title);
+    };
+
+    const handleClientesChange = (cliente) => {
+        setClienteSelecionado(cliente.nomePessoa);
+    };
+
+    const buscarServicos = async (idEmpresa) => {
+        try {
+            const response = await findServicos(idEmpresa);
+            setServicos(response.data);
+        } catch (error) {
+            console.error('Erro ao buscar os serviços:', error);
+        }
+    };
+
+    const buscarClientes = async (idEmpresa) => {
+        try {
+            const response = await findClientes(idEmpresa);
+            setClientes(response.data)
+        } catch (error) {
+
+        }
+    }
+
+    const handleClose = () => {
+        setIsVisible(false);
+        setTimeout(onClose, 400);
+    };
+
+    const isDisabled = () => {
+        if (
+            servicoSelecionado === '' ||
+            clienteSelecionado === '' ||
+            profissionalSelecionado === '' ||
+            !dataHoraAgendamento
+        ) {
+            return true;
+        }
+
+        const agora = dayjs(); // Data e hora atual
+        const agendamento = dayjs(dataHoraAgendamento); // Data e hora selecionada
+
+        if (agendamento.isBefore(agora)) {
+            return true;
+        }
+
+        return false;
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        alert(servicoSelecionado + clienteSelecionado + profissionalSelecionado + dataHoraAgendamento);
         try {
-            await cadastroService(formData);
-            alert('Cadastro realizado com sucesso!');
-            setFormData({
-                nomeServico: '',
-                valorServico: '',
-                representante: "true",
-                tempoExecucao: '',
-                categoria: '',
-            });
         } catch (error) {
             setErrorMessage('Erro ao cadastrar o serviço.');
         }
     };
 
     return (
-        <div className="modal-overlay">
-            <div className="modal-header">
-                <div className="container-modal">
-                    <h4 className="titulo-modal">Adicionar Novo Serviço <button className="botaoFechar" onClick={onClose}>X</button></h4>
-
-                    <form className="form-modal" onSubmit={handleSubmit}>
-                        <div className="form-group">
-                            <div className='inputLabel'>
-                                <label>Nome:</label>
-                                <input
-                                    type="text"
-                                    name="nomeServico"
-                                    placeholder="Nome do Serviço"
-                                    value={formData.nomeServico}
-                                    onChange={handleChange}
-                                    required
+        <div className={`modal-agendamento ${isVisible ? 'modal-aberto' : 'modal-fechado'}`}>
+            <div className="container-modal">
+                <h4 className="titulo-modal">Adicionar Novo Serviço<button className="botaoFechar" onClick={handleClose}>X</button></h4>
+                <form className="form-modal" onSubmit={handleSubmit}>
+                    <div className="form-agendamento">
+                        <div className='inputLabel'>
+                            <label>Cliente:</label>
+                            <div ref={dropdownRef}>
+                                <SearchableDropdown
+                                    options={clientes} // Lista de serviços
+                                    onSelectOption={handleClientesChange}
+                                    displayField={(option) => option.nomePessoa}
+                                    uniqueKey={(option) => option.idCliente}
                                 />
                             </div>
                         </div>
+                    </div>
 
-                        <div className="form-group">
-                            <div className='inputLabel'>
-                                <label>Valor:</label>
-                                <input
-                                    type="number"
-                                    name="valorServico"
-                                    placeholder="Valor do Serviço"
-                                    value={formData.valorServico}
-                                    onChange={handleChange}
-                                    required
+                    <div className="form-agendamento">
+                        <div className='inputLabel'>
+                            <label>Serviço:</label>
+                            <div ref={dropdownRef}>
+                                <SearchableDropdown
+                                    options={servicos}
+                                    placeholder={"Selecione um serviço"}
+                                    onSelectOption={handleServicoChange}
+                                    displayField={(option) => option.nomeServico}
+                                    uniqueKey={(option) => option.idServico}
                                 />
                             </div>
                         </div>
+                    </div>
 
-                        <div className="form-group">
-                            <div className='inputLabel'>
-                                <label>Tempo de Execução:</label>
-                                <input
-                                    type="text"
-                                    name="tempoExecucao"
-                                    placeholder="Tempo de Execução"
-                                    defaultValue={formData.tempoExecucao}
-                                    onChange={handleChange}
-                                    required
+                    <div className="form-agendamento">
+                        <div className='inputLabel'>
+                            <label>Profissional:</label>
+                            <div ref={dropdownRef}>
+                                <SearchableDropdown
+                                    options={funcionarios}
+                                    placeholder={"Selecione um profissional"}
+                                    onSelectOption={handleFuncionarioChange}
+                                    displayField={(option) => option.title}
+                                    uniqueKey={(option) => option.id}
                                 />
                             </div>
                         </div>
-
-                        <div className="form-group">
-                            <div className='inputLabel'>
-                                <label htmlFor="categoria">Categoria:</label>
-                                <select
-                                    id="categoria"
-                                    name="categoria"
-                                    defaultValue={formData.categoria}
-                                    onChange={handleChange}
-                                    required
-                                >
-                                    <option value="">Selecione a Categoria</option>
-                                    <option value="categoria1">Categoria 1</option>
-                                    <option value="categoria2">Categoria 2</option>
-                                    <option value="categoria3">Categoria 3</option>
-                                </select>
-                            </div>
-                        </div >
-
-                        <button className="botaoCadastrar" onClick={handleSubmit}>Cadastrar</button>
-                    </form >
-                </div>
+                    </div>
+                    <div className="form-agendamento">
+                        <div className='inputLabel'>
+                            <label htmlFor="categoria">Data e Hora do Agendamento:</label>
+                            <DateTimePickerOpenTo
+                                valordefault={new Date()}
+                                onChange={(newValue) => setDataHoraAgendamento(newValue)} // Atualiza o estado no componente pai
+                            />
+                        </div>
+                    </div >
+                    <div className="button-agendar">
+                        <Button
+                            type="submit"
+                            content="Agendar"
+                            backgroundColor='#2196f3'
+                            disabled={isDisabled()}
+                            fontSize='15px'
+                            onClick={handleSubmit}
+                            size='47%'
+                        />
+                    </div>
+                </form >
             </div>
         </div>
 
