6ca0b14212dc606984077c090cb9aac522cc2583
JuliaIFreitas <julia.freitas@sptech.school>
2024-10-15 10:23:03 -0300

    Inicei a tela de clientes

--- src/pages/Clientes/Clientes.jsx
+++ src/pages/Clientes/Clientes.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import './Clientes.css';
-import ModalAdd from "../../components/modalAdd/ModalAdd";
+// import ModalAdd from "../../components/modalAdd/ModalAdd";
 import Menu from "../../components/menu/Menu";
-import IconBreadcrumbs from "../../components/breadcrumb/Breadcrumb";
-import TelaServicos from "../../components/telaServicos/TelaServicos";
-import HeaderInterna from "../../components/headerInterna/HeaderInterna";
+import TelaClientes from "../../components/telaClientes/TelaClientes";
+import HeaderCliente from "../../components/headerCliente/HeaderCliente";
+// import HeaderInterna from "../../components/headerInterna/HeaderInterna";
 
 function Clientes() {
 
-    const camposCadastroServico = [
+    const camposCadastroCliente = [
         { label: 'Nome do Serviço', name: 'nomeServico', type: 'text', placeholder: 'Nome do Serviço', required: true },
         { label: 'Valor do Serviço', name: 'valorServico', type: 'number', placeholder: 'Valor do Serviço', required: true },
         { label: 'Tempo de Execução', name: 'tempoExecucao', type: 'text', placeholder: 'Tempo de Execução', required: true },
@@ -17,12 +17,12 @@ function Clientes() {
         { label: 'Descrição', name: 'descricao', type: 'textarea', placeholder: 'Descrição do Serviço', required: true },
     ];
 
-    const camposCadastroProduto = [
-        { label: 'Nome do Produto', name: 'nomeProduto', type: 'text', placeholder: 'Nome do Produto', required: true },
-        { label: 'Preço', name: 'preco', type: 'number', placeholder: 'Preço', required: true },
-        { label: 'Categoria', name: 'categoria', type: 'text', placeholder: 'Categoria do Produto', required: true },
-        { label: 'Descrição', name: 'descricao', type: 'textarea', placeholder: 'Descrição do Produto', required: true },
-    ];
+    // const camposCadastroProduto = [
+    //     { label: 'Nome do Produto', name: 'nomeProduto', type: 'text', placeholder: 'Nome do Produto', required: true },
+    //     { label: 'Preço', name: 'preco', type: 'number', placeholder: 'Preço', required: true },
+    //     { label: 'Categoria', name: 'categoria', type: 'text', placeholder: 'Categoria do Produto', required: true },
+    //     { label: 'Descrição', name: 'descricao', type: 'textarea', placeholder: 'Descrição do Produto', required: true },
+    // ];
 
     return (
         <div className="main-clientes">
@@ -38,19 +38,27 @@ function Clientes() {
                 />             */}
 
                 <div className="principal">
-                    <IconBreadcrumbs
-                        paths={[
-                            { label: 'Serviço', href: '/servico' }
-                        ]}
-                    />
 
                     <div className="titulo">
-                        <h3>Clientes</h3>
-                        <HeaderInterna texto="Novo Cliente" />
-                    </div>                
-                    
-                    <TelaServicos titulo="Serviços" placeholder="Pesquisar serviço" titulo1="NOME" titulo2="VALOR" titulo3="TEMPO" titulo4="CATEGORIA" titulo5="AÇÕES" />
+                        <div className="container-titulo">
+                            <h3>Clientes</h3>
+                            <HeaderCliente texto="Novo Cliente" />
+                        </div>
+                    </div>
 
+                    <TelaClientes titulo="Clientes" placeholder="Pesquisar cliente" titulo1="NOME" titulo2="TELEFONE" titulo3="EMAIL" titulo4="AÇÕES" />
+
+
+                    {/* {isModalOpen && (
+                        // <ModalAdd onClose={closeModalAdd} titulo="Adicionar Novo Serviço" />
+
+                        <ModalAdd
+                            titulo="Cadastrar Serviço"
+                            onClose={() => setIsModalOpen(false)}
+                            campos={camposCadastroServico}
+                            tituloBotao="Cadastrar"
+                        />
+                    )} */}
                 </div>
 
             </div>
