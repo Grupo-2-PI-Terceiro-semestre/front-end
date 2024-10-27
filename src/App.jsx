bd278c3ff16a18b8dbc8ac6a47d093cabed27032
JonathanCarvalho39 <jonathan.carvalho@sptech.school.com>
2024-10-27 14:55:17 -0300

    Ajuste na proteção das rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 // src/App.jsx
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, useLocation } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
-import Cookies from 'js-cookie';
 const projectName = 'Order Hub';
 const version = __APP_VERSION__;
 
@@ -29,27 +28,12 @@ const AppVersion = () => {
 };
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  useEffect(() => {
-    // Verifica se o cookie do token existe
-    const token = Cookies.get('token');
-
-    // Para fins de teste, vamos definir como verdadeiro
-    // Remova o comentário na linha abaixo se você realmente quer verificar o token
-    // if (token) {
-    //   setIsAuthenticated(true);
-    // }
-
-    setIsAuthenticated(true);
-
-  }, []);
 
   return (
     <Router>
       <div>
         <AppVersion />
-        <AppRoutes isAuthenticated={isAuthenticated} />
+        <AppRoutes/>
       </div>
     </Router>
   );
--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,10 +1,22 @@
 // src/ProtectedRoute.js
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
 
-const ProtectedRoute = ({ element, isAuthenticated }) => {
-    return isAuthenticated ? element : <Navigate to="/perfil" />;
+const ProtectedRoute = ({ element }) => {
+    const [isAuthenticated, setIsAuthenticated] = useState(null); // Inicializa com null
+
+    useEffect(() => {
+        const token = Cookies.get('token');
+        setIsAuthenticated(!!token); // Define como true/false dependendo da presença do token
+    }, []);
+
+    if (isAuthenticated === null) {
+        return <div>Loading...</div>;
+    }
+
+    return isAuthenticated ? element : <Navigate to="/login" replace />;
 };
 
 export default ProtectedRoute;
