7d059bb1c40e23faf580f4a70cda34c305749e99
JuliaIFreitas <julia.freitas@sptech.school>
2024-11-08 19:32:21 -0300

    Paginação da tela de clientes - botões, imagem de carregamento e mudando os clientes ao mudar a página

--- src/pages/Clientes/components/telaClientes/TelaClientes.jsx
+++ src/pages/Clientes/components/telaClientes/TelaClientes.jsx
@@ -1,43 +1,73 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import './TelaClientes.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import LinhaClientes from "../linhaClientes/LinhaClientes";
-// import HeaderInterna from "../headerInterna/HeaderInterna";
-import {findClientes} from "../../services/clienteServices";
+import { makeStyles } from "@mui/styles";
+import Pagination from "@mui/material/Pagination";
+import Stack from '@mui/material/Stack';
+import { findClientes } from "../../services/clienteServices";
 import Cookies from 'js-cookie';
+import CircularSize from '../../../../components/circulo-load/CircularSize';
 
 function TelaClientes({ placeholder, titulo1, titulo2, titulo3, titulo4
- }) {
+}) {
 
     const user = Cookies.get('user') ? JSON.parse(Cookies.get('user')) : null;
+    const [clientes, setClientes] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [totalPags, setTotalPags] = useState(0);
+    const [paginaAtual, setPaginaAtual] = useState(1);
+    const [loading, setLoading] = useState(false);
 
     const handleKeyUp = (event) => {
         const value = event.target.value;
         setSearchTerm(value);
         findByServicoOuEmpresa(value);
-      };
+    };
 
-      useEffect(() => {
-        buscarListaClientes(user.idEmpresa);
-      }, []);
-
-      const buscarListaClientes = async (idEmpresa) => {
-        const paginacao = {
-            pagina:null,
-            tamanho:null
+    useEffect(() => {
+        if (user && user.idEmpresa) {
+            buscarListaClientes(user.idEmpresa, paginaAtual, 8);
         }
-        const response = await findClientes(idEmpresa, paginacao);
-        console.log(response + ' response');
-      } 
+    }, [paginaAtual]);
+    
+
+    const buscarListaClientes = async (idEmpresa, pagina, tamanho) => {
+        try {
+            setLoading(true);
+            const paginacao = { pagina: pagina - 1, tamanho };
+            const response = await findClientes(idEmpresa, paginacao);
+
+            setClientes(response.data.itens);
+            console.log(JSON.stringify(response) + ' response');
+            console.log('set clientes ' + setClientes);
+
+            var totalItens = Number(response.data.totalItens);
+            var totalPagsCalc = Math.ceil(totalItens / tamanho);
+
+            setTotalPags(totalPagsCalc);
+            console.log(totalPagsCalc + ' pags');
+            console.log(paginaAtual + ' pag atual');
+
+        } catch (error) {
+            console.error('Erro ao buscar clientes:', error);
+            setClientes([]);
+            setTotalPags(0);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const handlePageChange = (event, page) => {
+        console.log(`Mudando para a página: ${page}`);
+        setPaginaAtual(page);
+        buscarListaClientes(user.idEmpresa, page, 8);  
+    };
 
     return (
         <div className="main-tela-clientes">
             <div className="container-tela-clientes">
-                {/* <div className="titulo">
-                    <h3>{titulo}</h3>
-                    <HeaderInterna texto="Novo Serviço"  />
-                </div> */}
 
                 <div className="search-box">
                     <FontAwesomeIcon icon={faMagnifyingGlass} className="search-icon" />
@@ -57,21 +87,36 @@ function TelaClientes({ placeholder, titulo1, titulo2, titulo3, titulo4
                     </div>
 
                     <div className="conjuntoLinhas">
-                        <LinhaClientes nome="Maria da Silva" telefone="(11)95521-5833" email="exemplo.text@gmail.com" />
-                        <LinhaClientes nome="Maria da Silva" telefone="(11)95521-5833" email="exemplo.text@gmail.com" />
-                        <LinhaClientes nome="Maria da Silva" telefone="(11)95521-5833" email="exemplo.text@gmail.com" />
-                        <LinhaClientes nome="Maria da Silva" telefone="(11)95521-5833" email="exemplo.text@gmail.com" />
-                        <LinhaClientes nome="Maria da Silva" telefone="(11)95521-5833" email="exemplo.text@gmail.com" />
-                        <LinhaClientes nome="Maria da Silva" telefone="(11)95521-5833" email="exemplo.text@gmail.com" />
-                        <LinhaClientes nome="Maria da Silva" telefone="(11)95521-5833" email="exemplo.text@gmail.com" />
-                        <LinhaClientes nome="Maria da Silva" telefone="(11)95521-5833" email="exemplo.text@gmail.com" />
-                        <LinhaClientes nome="Maria da Silva" telefone="(11)95521-5833" email="exemplo.text@gmail.com" />
-                        <LinhaClientes nome="Maria da Silva" telefone="(11)95521-5833" email="exemplo.text@gmail.com" />
-                        <LinhaClientes nome="Maria da Silva" telefone="(11)95521-5833" email="exemplo.text@gmail.com" />
-                        <LinhaClientes nome="Maria da Silva" telefone="(11)95521-5833" email="exemplo.text@gmail.com" />
+                        {Array.isArray(clientes) && clientes.length > 0 ? (
+                            clientes.map((cliente) => (
+                                <LinhaClientes
+                                    key={cliente.idCliente}
+                                    nome={cliente.nomePessoa}
+                                    telefone={cliente.telefone}
+                                    email={cliente.email}
+                                />
+                            ))
+                        ) : (
+                            <p className="erroCliente">Nenhum cliente encontrado.</p>
+                        )}
+                    </div>
+
+
+                    {loading ? (
+                        <CircularSize width="100%" height="100%" />
+                    ) : null}
+
+                    <div className="paginacao">
+                        <Stack spacing={2}>
+                            <Pagination
+                                count={totalPags}
+                                page={paginaAtual}
+                                onChange={handlePageChange}
+                                size="large"
+                            />
+                        </Stack>
                     </div>
                 </div>
-
 
             </div>
         </div>
