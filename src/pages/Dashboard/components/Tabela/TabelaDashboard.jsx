f92382dc2c14c0956d0d0a575899b49db21569a6
Joao Galhardo <joao.galhardo@sptech.school>
2024-11-20 16:13:16 -0300

    Fazendo a conexão das KPIs e da Tabela próximos Agendamentos

--- src/pages/Dashboard/components/Tabela/TabelaDashboard.jsx
+++ src/pages/Dashboard/components/Tabela/TabelaDashboard.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./TabelaDashboard.css";
+import {findListaAgendamentos} from "../../services/dashboardServices";
 
-function TabelaDashboard({ headers, data }) {
+function TabelaDashboard({ headers, idEmpresa }) {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        buscarListaAgendameno();
+    }, []);
+
+    const buscarListaAgendameno = async () => {
+        try {
+            const response = await findListaAgendamentos(idEmpresa);
+            setData(response);
+        } catch (error) {
+            console.error("Erro ao buscar os dados", error);
+        }
+    }
+
     return (
         <div className="tabela-dashboard-container">
             <table className="tabela-dashboard">
@@ -15,8 +31,8 @@ function TabelaDashboard({ headers, data }) {
                 <tbody>
                     {data.map((row, rowIndex) => (
                         <tr key={rowIndex} className="linha-tabela">
-                            {row.map((cell, cellIndex) => (
-                                <td key={cellIndex}>{cell}</td>
+                            {headers.map((header, cellIndex) => (
+                                <td key={cellIndex}>{row[header]}</td>
                             ))}
                         </tr>
                     ))}
--- src/pages/Dashboard/services/dashboardServices.js
+++ src/pages/Dashboard/services/dashboardServices.js
@@ -1,11 +1,32 @@
-import { getData } from '../../../router/router'
+    import { getData } from '../../../router/router'
 
-export const findDashboardData = async (idEmpresa, mes, endPoint) => {
-    try {
-        const response = await getData(`${endPoint}/${idEmpresa}`, {}, { mes });
-        return response.data;
-    } catch (error) {
-        console.error("Erro ao buscar o dashboard", error);
-        throw error;
+    export const findDashboardData = async (idEmpresa, mes, endPoint) => {
+        try {
+            const response = await getData(`${endPoint}/${idEmpresa}`, {}, { mes });
+            return response.data;
+        } catch (error) {
+            console.error("Erro ao buscar o dashboard", error);
+            throw error;
+        }
     }
-}
+
+    export const findListaAgendamentos = async (idEmpresa) => {
+        try {
+            const response = await getData(`agendamentos/${idEmpresa}`);
+            return response.data;
+        } catch (error) {
+            console.error("Erro ao buscar o dashboard", error);
+            throw error;
+        }
+    }
+
+    export const findChartClientData = async (idEmpresa,mes,endPoint) => {
+        try {
+            const response = await getData(`${endPoint}/${idEmpresa}`, {}, { mes });
+            return response.data;
+        } catch (error) {
+            console.error("Erro ao buscar o dashboard", error);
+            throw error;
+        }
+    }
+
