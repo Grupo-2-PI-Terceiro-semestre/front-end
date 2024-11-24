8d9a7b189e8e0bf4d90abd38fb985933f0f21662
JuliaIFreitas <julia.freitas@sptech.school>
2024-11-24 19:12:43 -0300

    Removendo comentários

--- src/pages/Perfil/components/formularioDadosPrincipais/FormularioPrincipal.jsx
+++ src/pages/Perfil/components/formularioDadosPrincipais/FormularioPrincipal.jsx
@@ -4,8 +4,7 @@ import InputMask from 'react-input-mask';
 import Cookies from 'js-cookie';
 import { atualizarDadosDePerfil } from '../../services/perfilServices';
 import CircularProgress from '@mui/material/CircularProgress';
-import { successToast, errorToast, infoToast } from '../../../../utils/Toats'
-  ;
+import { successToast, errorToast, infoToast } from '../../../../utils/Toats';
 import { set } from "react-hook-form";
 
 const FormularioPrincipal = ({ setRefreshKey }) => {
