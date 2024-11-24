f2dbd15415c5831c071fd97f26842994be99257b
JuliaIFreitas <julia.freitas@sptech.school>
2024-11-24 19:11:40 -0300

    Header arrumada

---
+++ src/pages/Equipe/components/headerInterna/HeaderInterna.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import './HeaderInterna.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import ModalAdd from "../../../../components/modalAdd/ModalAdd";
+import ModalAddCliente from "../../../Clientes/components/modalAddCliente/ModalAddCliente";
+import ModalAddEquipe from "../modalAddEquipe/ModalAddEquipe";
+
+function HeaderInterna({ texto }) {
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+
+    const openModalAdd = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModalAdd = () => {
+        setIsModalOpen(false);
+    };
+
+    return (
+        <div className="main-header-interna">
+            <div className="container-header-interna">
+                <div className="botao">
+                    <button onClick={openModalAdd}>
+                        <FontAwesomeIcon icon={faPlus} /> {texto}
+                    </button>
+                </div>
+
+                {isModalOpen && (
+
+                    <ModalAddEquipe
+                        titulo="Cadastrar Colaborador"
+                        onClose={() => setIsModalOpen(false)}
+                    />
+                )}
+
+            </div>
+
+
+        </div>
+    );
+}
+
+export default HeaderInterna;
