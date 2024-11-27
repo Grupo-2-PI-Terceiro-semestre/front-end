bbea8ae341188da9af94a59138862fccef92a0b2
JuliaIFreitas <julia.freitas@sptech.school>
2024-11-27 00:10:43 -0300

    Conexão feita com o cadastro - back end

--- src/pages/Equipe/components/modalAddEquipe/ModalAddEquipe.jsx
+++ src/pages/Equipe/components/modalAddEquipe/ModalAddEquipe.jsx
@@ -11,25 +11,23 @@ import SearchableDropdown from '../autocomplete/SearchableDropdown';
 // import ModalAddFuncao from "../modalAddFuncao/ModalAddFuncao";
 
 function ModalAddEquipe({ onClose, titulo }) {
-    const [isVisibleAdd, setIsVisibleAdd] = useState(false);
     const [loading, setLoading] = useState(false);
     const user = Cookies.get('user') ? JSON.parse(Cookies.get('user')) : null;
-    const [clientes, setClientes] = useState([]);
-    const [totalPags, setTotalPags] = useState(0);
-    const [paginaAtual, setPaginaAtual] = useState(1);
+
+    const [isVisibleAdd, setIsVisibleAdd] = useState(false);
+
+    const [nomePessoaDigitado, setNomePessoaDigitado] = useState('');
+    const [numeroTelefoneDigitado, setNumeroTelefoneDigitado] = useState('');
+    const [emailDigitado, setEmailDigitado] = useState('');
+    const [funcaoSelecionada, setFuncaoSelecionada] = useState(null);
     const [funcoes, setFuncoes] = useState([]);
-    const [isModalOpenAddFuncao, setIsModalOpen] = useState(false);
-    const [funcaoSelecionado, setFuncaoSelecionada] = useState('');
 
-    const [nomeSelecionado, setNomeSelecionado] = useState('');
-
-
-    const [formData, setFormData] = useState({
-        nomePessoa: '',
-        numeroTelefone: '',
-        nomeFuncao: '',
-        tiposDeUsuario: 'ADMIN'
-    });
+    // const [formData, setFormData] = useState({
+    //     nomePessoa: '',
+    //     numeroTelefone: '',
+    //     nomeFuncao: '',
+    //     tiposDeUsuario: 'ADMIN'
+    // });
 
     useEffect(() => {
         setIsVisibleAdd(true);
@@ -37,56 +35,30 @@ function ModalAddEquipe({ onClose, titulo }) {
     }, []);
 
     const handleClose = () => {
-        setIsVisibleAdd(false); // Inicia a animação de saída
         setTimeout(onClose, 300); // Fecha o modal após a animação
     };
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
+    // Atualiza o estado para o nome
+    const handleNomeChange = (event) => {
+        setNomePessoaDigitado(event.target.value);
     };
 
-    const criarColaborador = async (colaborador, idEmpresa) => {
-        setLoading(true);
-        try {
-            await createColaborador(colaborador, idEmpresa);
-            successToast('Colaborador criado com sucesso!');
-
-            console.log("Colaborador criado com sucesso:", colaborador);
-
-            buscarListaUsuarios(user.idEmpresa, paginaAtual, 8);
-
-            setTimeout(() => onCloseEquipe(), 3000);
-        } catch (error) {
-            errorToast('Colaborador não criado');
-
-        } finally {
-            setLoading(false);
-        }
+    // Atualiza o estado para o telefone
+    const handleTelefoneChange = (event) => {
+        setNumeroTelefoneDigitado(event.target.value);
     };
 
-    const handleSubmit = (event) => {
-
-        const colaboradorData = {
-            // idCliente: clienteSelecionado,
-            // idServico: servicoSelecionado,
-            // idAgenda: profissionalSelecionado,
-            // dataAgendamento: converterGMTParaBrasilia(dataHoraAgendamento),
-            // statusAgendamento: 'PENDENTE'
-
-            nomePessoa: nomePessoa
-        }
-
-        console.log(colaboradorData + ' colaborador data');
-
-        event.preventDefault();
-
-        const idEmpresa = user.idEmpresa;
-
-        criarColaborador(formData, idEmpresa);
-
+    const handleEmailChange = (event) => {
+        setEmailDigitado(event.target.value);
     };
 
+    // Atualiza o estado para a função selecionada no dropdown
+    const handleFuncoesChange = (funcao) => {
+        setFuncaoSelecionada(funcao);
+    };
+
+
+    // Busca as funções disponíveis para o dropdown
     const buscarFuncoes = async () => {
         try {
             const response = await findFuncoes();
@@ -99,48 +71,32 @@ function ModalAddEquipe({ onClose, titulo }) {
         }
     }
 
-    const buscarListaUsuarios = async (idEmpresa, pagina, tamanho) => {
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if (!nomePessoaDigitado || !numeroTelefoneDigitado || !emailDigitado || !funcaoSelecionada) {
+            errorToast('Por favor, preencha todos os campos.');
+            return;
+        }
+
+        const colaboradorData = {
+            nomePessoa: nomePessoaDigitado,
+            numeroTelefone: numeroTelefoneDigitado,
+            emailPessoa: emailDigitado,
+            idFuncao: funcaoSelecionada.idFuncao, // ID da função selecionada
+        };
+
         try {
             setLoading(true);
-            const paginacao = { pagina: pagina - 1, tamanho };
-            const response = await findUsuarios(idEmpresa, paginacao);
-
-            setUsuarios(response.data.itens);
-            console.log(JSON.stringify(response) + ' response');
-            console.log('set usuarios ' + setUsuarios);
-
-            var totalItens = Number(response.data.totalItens);
-            var totalPagsCalc = Math.ceil(totalItens / tamanho);
-
-            setTotalPags(totalPagsCalc);
-            console.log(totalPagsCalc + ' pags');
-            console.log(paginaAtual + ' pag atual');
-
+            const idEmpresa = user?.idEmpresa;
+            await createColaborador(colaboradorData, idEmpresa);
+            successToast('Colaborador criado com sucesso!');
+            onClose(); // Fecha o modal após sucesso
         } catch (error) {
-            console.error('Erro ao buscar usuários:', error);
-            setServicos([]);
-            setTotalPags(0);
+            errorToast('Erro ao criar colaborador.');
         } finally {
             setLoading(false);
         }
-    };
-
-    // const handleNomeChange (nomePessoa) => {
-    //     setNomeSelecionado(funcao.nomePessoa);
-    // }
-
-    const handleFuncoesChange = (funcao) => {
-        setFuncaoSelecionada(funcao.idFuncao);
-        nomeFuncao = funcao.nomeFuncao;
-        console.log(nomeFuncao + ' nome da função no handle change');
-    };
-
-    const openModalAddFuncao = () => {
-        setIsModalOpen(true);
-    };
-
-    const closeModalAddFuncao = () => {
-        setIsModalOpen(false);
     };
 
     return (
@@ -157,7 +113,7 @@ function ModalAddEquipe({ onClose, titulo }) {
                                     type="text"
                                     name="nomePessoa"
                                     placeholder="Nome do Colaborador"
-                                    value={nomePessoa}
+                                    value={nomePessoaDigitado}
                                     onChange={handleNomeChange}
                                     required
                                 />
@@ -172,67 +128,64 @@ function ModalAddEquipe({ onClose, titulo }) {
                                     type="text"
                                     name="numeroTelefone"
                                     placeholder="(XX)XXXXX-XXXX"
-                                    value={formData.numeroTelefone}
-                                    onChange={handleChange}
+                                    value={numeroTelefoneDigitado}
+                                    onChange={handleTelefoneChange}
                                     required
                                 />
-
                             </div>
                         </div>
 
                         <div className="form-grupo-serv">
                             <div className='inputLabel'>
-                                <label>Função</label>
+                                <label>E-mail:</label>
+                                <input
+                                    className="input"
+                                    type="email"
+                                    name="emailPessoa"
+                                    placeholder="Digite o email"
+                                    value={emailDigitado}
+                                    onChange={handleEmailChange}
+                                    required
+                                />
+                            </div>
+                        </div>
+
+                        <div className="form-grupo-serv">
+                            <div className='inputLabel'>
+                                <label>Função:</label>
                                 <SearchableDropdown className="input-cliente"
                                     name="nomeFuncao"
                                     options={funcoes}
                                     required={true}
-                                    value={formData.nomeFuncao}
+                                    value={funcaoSelecionada?.nomeFuncao || ''}
                                     onSelectOption={handleFuncoesChange}
                                     displayField={(option) => option.nomeFuncao}
                                     uniqueKey={(option) => option.idFuncao}
                                 />
+
+                                {/* <SearchableDropdown 
+                                    className="input-cliente"
+                                    name="nomeFuncao"
+                                    options={funcoes}
+                                    required={true}
+                                    value={nomeFuncao}
+                                    onSelectOption={handleFuncoesChange}
+                                    displayField={(option) => option.nomeFuncao}
+                                    uniqueKey={(option) => option.idFuncao}
+                                /> */}
                             </div>
-
-                            {/* <Tooltip title="Adicionar Cliente" arrow>
-                                <FontAwesomeIcon icon={faPlus} onClick={openModalAddFuncao} className="icon-plus" />
-                            </Tooltip> */}
-
                         </div>
-
-                        {/* <div className='inputLabel'>
-                            <label>Cliente: </label>
-                            <div className="cliente-plus">
-                                <div className="input-cliente-plus">
-                                    <SearchableDropdown className="input-cliente"
-                                        options={clientes}
-                                        required={true}
-                                        value={''}
-                                        onSelectOption={handleClientesChange}
-                                        displayField={(option) => option.nomePessoa}
-                                        uniqueKey={(option) => option.idCliente}
-                                    />
-                                </div>
-
-
-                            </div>
-                        </div> */}
-
 
                         <div className="botao-add-serv">
-                            <button type="submit" className="botaoCadastrar">
-                                Cadastrar
+                            <button type="submit" className="botaoCadastrar" disabled={loading}>
+                                {loading ? 'Cadastrando...' : 'Cadastrar'}
                             </button>
                         </div>
-                    </form >
+                    </form>
                 </div>
             </div>
-
-            {/* {isModalOpenAddFuncao && (
-                <ModalAddFuncao onClose={closeModalAddFuncao} />
-            )} */}
         </div>
-    )
+    );
 }
 
 export default ModalAddEquipe;
